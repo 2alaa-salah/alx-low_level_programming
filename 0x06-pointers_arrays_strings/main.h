07e151f454b54cc1137595ed3be691fae64504da
2alaa-salah <asalah0112107@gmail.com>
2023-03-03 17:01:51 +0200

    ./1-strncat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,5 +15,5 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 char *_strcat(char *dest, char *src);
-
+char *_strncat(char *dest, char *src, int n);
 #endif
