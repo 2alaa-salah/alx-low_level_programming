8a3f8363a8e43d59667466684ade8c0abe57afac
2alaa-salah <asalah0112107@gmail.com>
2023-04-13 16:08:17 +0200

    first file 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,24 +6,21 @@
  *
  * Return: the converted number
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	if (b == NULL)
-		return (0);
-
-	unsigned int res;
-
-	res = 0;
-
 	int i;
+	unsigned int dec_val = 0;
+
+	if (!b)
+		return (0);
 
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] > '1' || b[i] < '0')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-
-		res = res * 2 + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (res);
+
+	return (dec_val);
 }
+
